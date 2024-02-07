399c4c9305fcf44176f365db30ec9ea91f3792b7
Krzysztof Stronka <kstronka@apptension.com>
2024-02-07 22:11:26 +0100

    override

--- injection/src/injection/di.py
+++ injection/src/injection/di.py
@@ -1,6 +1,6 @@
 from typing import TypeVar
+from contextlib import contextmanager
 
-T = TypeVar('T')
 
 _registry = {}
 
@@ -10,9 +10,29 @@ def get_from_registry(interface: type) -> type:
 
 
 def implements(*, interface: type):
-    def decorator(impl: type):
+    def decorator(impl: type | object):
         _registry[interface] = impl
         return impl
 
     return decorator
 
+
+class Require:
+    def __init__(self, interface):
+        self._interface = interface
+
+    def __get__(self, instance, owner):
+        maybe_cls = _registry[self._interface]
+
+        if isinstance(maybe_cls, type):
+            return maybe_cls()
+
+        return maybe_cls
+
+
+@contextmanager
+def override(interface: type, *, using):
+    old = get_from_registry(interface)
+    implements(interface=interface)(using)
+    yield
+    implements(interface=interface)(old)
--- injection/tests/test_di.py
+++ injection/tests/test_di.py
@@ -1,6 +1,6 @@
 from typing import Protocol
 
-from src.injection.di import get_from_registry, implements
+from src.injection.di import get_from_registry, implements, Require, override
 
 
 class SimpleProtocol(Protocol):
@@ -16,4 +16,44 @@ class TestInjectionDi:
                 return message
 
         assert issubclass(get_from_registry(SimpleProtocol), Echo)
+
+    def test_require_always_get_implementation(self):
+        @implements(interface=SimpleProtocol)
+        class Echo:
+            def echo(self, message: str):
+                return message
+
+        class Container:
+            dep: SimpleProtocol = Require(SimpleProtocol)
+
+        assert isinstance(Container.dep, Echo)
+
+    def test_require_always_behaves_okay(self):
+        @implements(interface=SimpleProtocol)
+        class Echo:
+            def echo(self, message: str):
+                return message
+
+        class Container:
+            dep: SimpleProtocol = Require(SimpleProtocol)
+
+        assert Container.dep.echo("hello") == "hello"
+
+    def test_override_always_replaces_impl(self, mocker):
+        @implements(interface=SimpleProtocol)
+        class Echo:
+            def echo(self, message: str):
+                return message
+
+        class Container:
+            dep: SimpleProtocol = Require(SimpleProtocol)
+
+        spy = mocker.MagicMock()
+
+        with override(SimpleProtocol, using=spy):
+            Container.dep.echo("hello")
+
+        spy.echo.assert_called_once_with("hello")
+
+
 

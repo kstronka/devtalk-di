17ffc7cade80f122904b50c3ba4b0e274f423148
Krzysztof Stronka <kstronka@apptension.com>
2024-02-07 22:00:54 +0100

    implements

--- injection/src/injection/di.py
+++ injection/src/injection/di.py
@@ -1,2 +1,18 @@
-def foo():
-    return 1
+from typing import TypeVar
+
+T = TypeVar('T')
+
+_registry = {}
+
+
+def get_from_registry(interface: type) -> type:
+    return _registry[interface]
+
+
+def implements(*, interface: type):
+    def decorator(impl: type):
+        _registry[interface] = impl
+        return impl
+
+    return decorator
+
--- injection/tests/test_di.py
+++ injection/tests/test_di.py
@@ -1,6 +1,19 @@
-from src.injection.di import foo
+from typing import Protocol
+
+from src.injection.di import get_from_registry, implements
+
+
+class SimpleProtocol(Protocol):
+    def echo(self, message: str) -> str:
+        ...
 
 
 class TestInjectionDi:
-    def test_di(self):
-        assert foo() == 1
+    def test_implements_always_register_implementation_for_protocol(self):
+        @implements(interface=SimpleProtocol)
+        class Echo:
+            def echo(self, message: str):
+                return message
+
+        assert issubclass(get_from_registry(SimpleProtocol), Echo)
+
